72badde0526b741880fb0accb4c0e1a7de5e6820
Juvraj Singh <juvraj@gmail.com>
2025-12-08 20:57:14 +0000

    POST code started for after form submitted - populating matches table

--- app.py
+++ app.py
@@ -13,17 +13,41 @@ creds = Credentials.from_service_account_file(
 gc = gspread.authorize(creds)
 
 sh = gc.open_by_key("1XuTyJb0PG8OIEHj-UosEqXW0m8mNu3iQUvJUXM8tw1E")
+
 players_sheet = sh.worksheet("Players")
+matches_sheet = sh.worksheet("Matches")
+appearances_sheet = sh.worksheet("Appearances")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # TO DO
+    # If user submits form
+    if request.method == "POST":
+        match_id = 1 # match_id TO DO
+        match_date = request.form.get("match_date")
+        team = request.form.get("team")
+        opponent = request.form.get("opponent")
+        venue = request.form.get("venue")
+        season = "" # left empty so that arrayformula in this column works
+        goals_for = request.form.get("goals_scored")
+        goals_against = request.form.get("goals_conceded")
+
+        matches_new_row = [match_id, match_date, team, opponent, venue, season, goals_for, goals_against]
+
+        for column in matches_new_row:
+            if not column:
+                return render_template("index.html") # TO CHANGE
+        
+        matches_sheet.append_row(matches_new_row)
+
+        return render_template("index.html") # TO CHANGE
+    
+    # If user opens form
     return render_template("index.html")
 
-# Autocomplete for when user starts typing in name in players section
 @app.route("/autocomplete")
 def autocomplete():
+    """Autocomplete for when user starts typing in name in players section"""
     query = request.args.get("query", "").strip().lower()
 
     if not query:
